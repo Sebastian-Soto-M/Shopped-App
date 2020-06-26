33f99fd46d4949dc091680a378c54c971b1f9bff
Sebastian Noe Soto Madrigal <ssotom@ucenfotec.ac.cr>
2020-06-26 16:42:03 -0600

    App wsgi

--- main/__init__.py
+++ main/__init__.py
@@ -1,39 +1,44 @@
-import os
-import jinja2
-import cloudinary
+# import os
+
+# import cloudinary
+# import jinja2
+# from flask import Flask
+# from flask_bcrypt import Bcrypt
+# from flask_login import LoginManager
+
+
+# def init_couldinary():
+# CLOUDINARY_CLOUD_NAME = 'snsm-img'
+# CLOUDINARY_API_KEY = '539871227625762'
+# CLOUDINARY_API_SECRET = '1I3msDYBT5xsMZPbWAOFqkpdABI'
+
+# cloudinary.config(
+# cloud_name=CLOUDINARY_CLOUD_NAME,
+# api_key=CLOUDINARY_API_KEY,
+# api_secret=CLOUDINARY_API_SECRET
+# )
+
+
+# def create_app(conf: config.Config = config.DevelopmentConfig):
+# app = Flask(__name__)
+# # app.config.from_pyfile(os.path.join('..', 'config.cfg'))
+# # template_dir = os.path.join('.', 'templates')
+# # loader = jinja2.FileSystemLoader(template_dir)
+# # environment = jinja2.Environment(loader=loader)
+# # bcrypt = Bcrypt(app)
+# # login_manager = LoginManager(app)
+# # login_manager.login_view = 'login'
+# # login_manager.login_message_category = 'info'
+# with app.app_context():
+# import main.routes
 from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-
-# Constants
-CLOUDINARY_CLOUD_NAME = 'snsm-img'
-CLOUDINARY_API_KEY = '539871227625762'
-CLOUDINARY_API_SECRET = '1I3msDYBT5xsMZPbWAOFqkpdABI'
-
-# Init flask app
 app = Flask(__name__)
-app.config.from_pyfile(os.path.join('..', 'config.cfg'))
 
-# Init jinja environment
-template_dir = os.path.join('.', 'templates')
-loader = jinja2.FileSystemLoader(template_dir)
-environment = jinja2.Environment(loader=loader)
 
-# Init bcrypt
-bcrypt = Bcrypt(app)
+@app.rounte('/')
+def hello():
+    return "hola jime"
 
-# Init login_manager
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
 
-# Init Cloudinary
-cloudinary.config(
-    cloud_name=CLOUDINARY_CLOUD_NAME,
-    api_key=CLOUDINARY_API_KEY,
-    api_secret=CLOUDINARY_API_SECRET
-)
-
-# Init routes
-with app.app_context():
-    import main.routes
+if __name__ == "__main__":
+    app.run()
