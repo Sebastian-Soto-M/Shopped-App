6ef0b8b5c5c25d9ff60fd96400c4a93e444711a3
Sebastian Noe Soto Madrigal <ssotom@ucenfotec.ac.cr>
2020-07-03 00:41:19 -0600

    Fix: Migrated to bs4, New: Using MDBootstrap, Fix: Base routes and html

--- main/routes/base.py
+++ main/routes/base.py
@@ -3,19 +3,18 @@ from flask import Blueprint, redirect, render_template, url_for, request
 r_base = Blueprint('r_base', __name__, static_folder='static')
 
 
-@r_base.route('/')
-@r_base.route('/signin')
-def signin():
-    return render_template('views/base/signin.html', img='bgimg.jpg')
+@r_base.route('/login')
+def login():
+    return render_template('views/base/login.html', img='bgimg.jpg')
 
 
-@r_base.route('/signup')
-def signup():
-    return render_template('views/base/index.html')
+@r_base.route('/register')
+def logup():
+    return render_template('views/base/register.html')
 
 
-@r_base.route('/signout')
-def signout():
+@r_base.route('/logout')
+def logout():
     return render_template('views/base/index.html')
 
 
@@ -24,6 +23,6 @@ def account():
     return render_template('views/base/index.html')
 
 
-# @r_base.route('/')
-# def home():
-    # return render_template('views/base/index.html')
+@r_base.route('/')
+def home():
+    return render_template('views/base/index.html')
