ddae9b0935da25453ab5785a867ad6242842b8f6
Jimena <jmoraz1@ucenfotec.ac.cr>
2020-07-27 01:46:08 -0600

    feat: adding test for api requests

--- main/routes/recipe.py
+++ main/routes/recipe.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, redirect, render_template, url_for, request
 from ..forms import RecipeForm
+import requests
+import json
+
 
 r_recipe = Blueprint('r_recipe', __name__,
                    static_folder='static')
@@ -7,8 +10,12 @@ r_recipe = Blueprint('r_recipe', __name__,
 @r_recipe.route('/recipe', methods=['GET', 'POST'])
 def recipe():
     form = RecipeForm()
+    response = requests.get('http://ec2-54-184-147-64.us-west-2.compute.amazonaws.com:8080/shopped-api/api/v1/recipe')
+    print (json.loads(response.text))
     if form.validate_on_submit():
+     
+       
         print('saving recipe')
-        return redirect(url_for('.recipes'))
+        return redirect(url_for('.recipe'))
    
     return render_template('views/base/recipe.html', title='Recipes', form=form,img='recipe.jpg')
