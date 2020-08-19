47451df5212e6ccd766e6ee56e4601d17530e753
Jimena <jmoraz1@ucenfotec.ac.cr>
2020-08-18 20:54:34 -0600

    feat: adding new views/routes for listing recipes

--- main/routes/recipe.py
+++ main/routes/recipe.py
@@ -13,9 +13,6 @@ r_recipe = Blueprint('r_recipe', __name__,
 @r_recipe.route('/recipe', methods=['GET', 'POST'])
 def recipe():
     form = RecipeForm()
-    response = requests.get(
-        'http://ec2-54-184-147-64.us-west-2.compute.amazonaws.com:8080/shopped-api/api/v1/recipe')
-    print(json.loads(response.text))
 
     if form.validate_on_submit():
         instructions = form.instructions.data.splitlines()
@@ -41,3 +38,11 @@ def recipe():
         return redirect(url_for('.recipe'))
 
     return render_template('views/base/recipe.html', title='Recipes', form=form, img='recipe.jpg')
+
+@ r_recipe.route('/discover_recipes')
+def discover_recipes():
+    response = requests.get(
+            'http://ec2-54-184-147-64.us-west-2.compute.amazonaws.com:8080/shopped-api/api/v1/recipe')
+    data=json.loads(response.text)
+
+    return render_template('views/base/discover_recipes.html', title='Recipes', data=data)
